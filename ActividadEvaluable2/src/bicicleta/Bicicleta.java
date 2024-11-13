c462aa775477e168e2d2a8fb7c91cfb655e74019
sammytsukino <samy.cabello.g@gmail.com>
2024-11-13 09:44:26 +0100

    eing

---
+++ ActividadEvaluable2/src/bicicleta/Bicicleta.java
@@ -0,0 +1,24 @@
+package bicicleta;
+
+public class Bicicleta {
+	
+	public enum tipo {MONTAÑA, CARRETERA, CIUDAD}
+	public enum tamaño {NIÑO, ADULTO}
+	
+	private String color;
+	private int pulgadas;
+	private String marca;
+	private boolean esElectrica;
+	private float precio;
+	private boolean tieneLuces;
+	private tipo tipo;
+	private tamaño tamaño;
+	
+	
+	public Bicicleta(String color, int pulgadas, String marca, boolean esElectrica, float precio, boolean tieneLuces, tipo tipo, tamaño tamaño) {
+		
+	
+	
+}
+
+}
