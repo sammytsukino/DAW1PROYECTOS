af3ee9e33da48a368198f819a321c4ebe62b7fad
sammytsukino <samy.cabello.g@gmail.com>
2025-01-13 10:24:24 +0100

    13/01 exercises

---
+++ Sandbox/Programación/Unidad-3/src/Ternario.java
@@ -0,0 +1,18 @@
+public class Ternario {
+
+    public static void main(String[] args) {
+        int calificacion = 3;
+
+        if (calificacion >= 5) {
+            System.out.println("Aprobado");
+        } else {
+            System.out.println("Suspenso");
+        
+    }
+
+    System.out.println(calificacion >= 5 ? "Has aprobado" : "Has suspendido");
+
+    }
+
+}
+
